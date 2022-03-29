dc08066bab80319f671af244c734c3add58e0431
Janka K <janakorycanova@gmail.com>
2022-03-29 19:17:00 +0200

    feature first animal  added to the farm

--- 02-farma-zvirat/main.js
+++ 02-farma-zvirat/main.js
@@ -14,24 +14,41 @@ let ovecka = {
 
     //let zvire =
 
+    let zvire = document.createElement('div');
+    zvire.className = 'zvire'; // prirazeni tridy pro element
+
+
+ 
 
 // 2. Nyni vytvor dva elementy pro obrazek a jmeno zvirete, ktere budou uvnitr naseho obalujiciho div elementu zvire.
     //2.1. Vytvor img element s tridou 'foto'. Jako src obrazku pridej hodnotu objektu krava.
 
     //let fotoZvirete =
 
+    let fotoZvire = document.createElement('img');
+    fotoZvire.className = 'foto';
+    fotoZvire.src = krava.foto; 
+
     //2.2. Dale vytvor div element s tridou 'jmeno'. Jako text elementu zobraz hodnotu vlastnosti jmeno objektu krava.
 
     //let jmenoZvirete =
 
+    let jmenoZvire = document.createElement('span');
+    jmenoZvire.className = 'jmeno';
+    jmenoZvire.innerHTML = krava.jmeno;
+
     //2.3. Zabal fotoZvire a jmenoZvirete do obalovaciho divu zvire, (psst zvire.appendChild)
 
+    zvire.appendChild(fotoZvire);
+    zvire.appendChild(jmenoZvire);
 
 // 3. Nakonec pojdme nas kod zapojit do html stranky. Na html strance vyber existujici element s tridou farma.
 // a vloz do nej obalujici element zvire (pssst appendChild)
 
   //let farma =
 
+  let farma = document.querySelector(".farma");
+  farma.appendChild(zvire);
 
 // Bonus
 // Co kdybychom meli program, kde tech zvirat je vic - objekt krava, objekt ovce, objekt prase...

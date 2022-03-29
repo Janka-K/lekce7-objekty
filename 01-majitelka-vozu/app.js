e2f94a61ce8ab7d7dcd4d064d20447060109b824
Janka K <janakorycanova@gmail.com>
2022-03-29 18:47:50 +0200

    Feature car owner data structure

--- 01-majitelka-vozu/app.js
+++ 01-majitelka-vozu/app.js
@@ -1,10 +1,24 @@
 // Vytvoř objekt auto, který bude mít vlastnosti: znacka, barva, rokVyroby,..
 
-
+let auto = {
+    znacka: 'Ford',
+    barva: 'modra',
+    rok_vyroby: 2015,
+    spz: '1b23456'
+};
 // Přidej novou vlastnost (majitel a prirad ji svoje jmeno jako hodnotu)
+
+auto.majitel = {};
+auto.majitel.jmeno = 'Jana';
+auto.majitel.prijmeni = 'Novakova';
 
 
 // Přepiš značku na Škoda
 
+auto.znacka = 'Skoda';
 
 // Vytvor tri promenne majitelka, znacka a spz a vloz do techto elementu text z objektu auto.
+
+let znacka = auto.znacka;
+let spz = auto.spz;
+let majitelka = auto.majitel.jmeno + ' ' + auto.majitel.prijmeni;
